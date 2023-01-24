03890f160b1613562dd91b3259a96eca3e0a8b93
IQuesadilla <shologuckers.iq@gmail.com>
2023-01-24 10:51:21 -0700

    fixed the streaming

--- logcpp.cpp
+++ logcpp.cpp
@@ -2,11 +2,6 @@
 #include "sstream"
 #include <iostream>
 
-void lifetimelogcpp::operator<<(const char *output)
-{
-    logobj->log(output);
-}
-
 logcpp::logcpp()
 {
     tabs = 0;
@@ -19,14 +14,42 @@ logcpp::~logcpp()
 
 void logcpp::log(const char *output)
 {
-    indent();
-    std::cout << "Log: " << output << std::endl;
+    logstream << output;
+}
+
+void logcpp::flush(loglevel lev)
+{
+    std::cout << indent();
+    switch (lev)
+    {
+        case NOTE:
+            std::cout << "Note: ";
+            break;
+        case VALUE:
+            std::cout << "Value: ";
+            break;
+        case WARNING:
+            std::cout << "Warning: ";
+            break;
+        case ERROR:
+            std::cout << "ERROR: ";
+            break;
+        case FUNCTION:
+            std::cout << "Function: ";
+            break;
+    }
+
+    std::cout << logstream.str() << std::endl;
+
+    logstream.str("");
+    logstream.clear();
 }
 
 lifetimelogcpp logcpp::function(const char *name)
 {
     indent();
-    std::cout << "Function: (" << name << ")" << std::endl;
+    logstream << "(" << name << ")";
+    flush(loglevel::FUNCTION);
     ++tabs;
     return lifetimelogcpp(this);
 }
@@ -36,12 +59,37 @@ void logcpp::endfunc()
     --tabs;
 }
 
-void logcpp::indent()
+std::string logcpp::indent()
 {   
     std::stringstream os;
     for (int i = 0; i < tabs; ++i)
     {
         os << "  ";
     }
-    std::cout << os.str();
+    return os.str();
 }
+
+lifetimelogcpp & operator<<(lifetimelogcpp &buff, const char *output)
+{
+    buff.logobj->log(output);
+    return buff;
+}
+
+lifetimelogcpp & operator<<(lifetimelogcpp &buff, const std::string output)
+{
+    buff.logobj->log(output.c_str());
+    return buff;
+}
+
+lifetimelogcpp & operator<<(lifetimelogcpp &buff, const int output)
+{
+    buff.logobj->log(std::to_string(output).c_str());
+    return buff;
+}
+
+lifetimelogcpp & operator<<(lifetimelogcpp &buff, logcpp::loglevel lev)
+{
+    buff.logobj->flush(lev);
+    return buff;
+}
+
--- logcpp.h
+++ logcpp.h
@@ -2,20 +2,34 @@
 #define LOGCPP_H
 #pragma once
 
+#include <sstream>
+
 class lifetimelogcpp;
 
 class logcpp
 {
 public:
+    enum loglevel
+    {
+        NOTE,
+        VALUE,
+        WARNING,
+        ERROR,
+        FUNCTION
+    };
+
     logcpp();
     ~logcpp();
 
     void log(const char *output);
+    void flush(loglevel lev);
     lifetimelogcpp function(const char *name);
     void endfunc();
 
 private:
-    void indent();
+    std::string indent();
+
+    std::stringstream logstream;
     int tabs;
 };
 
@@ -25,7 +39,10 @@ public:
     lifetimelogcpp(logcpp *log) { logobj = log; }
     ~lifetimelogcpp() { logobj->endfunc(); }
 
-    void operator<<(const char *output);
+    friend lifetimelogcpp & operator<<(lifetimelogcpp &buff, const char *output);
+    friend lifetimelogcpp & operator<<(lifetimelogcpp &buff, const std::string output);
+    friend lifetimelogcpp & operator<<(lifetimelogcpp &buff, const int output);
+    friend lifetimelogcpp & operator<<(lifetimelogcpp &buff, const logcpp::loglevel lev);
 
 private:
     logcpp *logobj;
